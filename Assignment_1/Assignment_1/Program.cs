a73b5531f93d22fd3643a8c4dc52c809bfae7eac
Gurpreet <bhangu_gurpreet@yahoo.com>
2020-10-20 17:03:52 -0400

    initail commit

---
+++ Assignment_1/Assignment_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Enter a series of numbers, separated by a +");
+            string input = Console.ReadLine();
+             if(input.Contains("+"))
+             { 
+                  string[] splittedInput = input.Split('+');
+                  foreach (var number in splittedInput)
+                  {
+                      int numberInt = Int32.Parse(number);
+                      sum = sum + numberInt;
+                  }
+                  Console.WriteLine(sum);
+             }
+            else
+            {
+                Console.WriteLine("Invalid Input, Numbers must be separated by a +");
+            }
+        }
+    }
+}
