28d81327ed5656de1be349bb4cbe42a781706631
Gurpreet <bhangu_gurpreet@yahoo.com>
2020-10-21 08:36:40 -0400

    exception handling added

--- Assignment_1/Assignment_1/Program.cs
+++ Assignment_1/Assignment_1/Program.cs
@@ -9,19 +9,26 @@ namespace Assignment_1
             int sum = 0;
             Console.WriteLine("Enter a series of numbers, separated by a +");
             string input = Console.ReadLine();
-             if(input.Contains("+"))
-             { 
-                  string[] splittedInput = input.Split('+');
-                  foreach (var number in splittedInput)
-                  {
-                      int numberInt = Int32.Parse(number);
-                      sum = sum + numberInt;
-                  }
-                  Console.WriteLine(sum);
-             }
-            else
+            try
             {
-                Console.WriteLine("Invalid Input, Numbers must be separated by a +");
+                if (input.Contains("+"))            
+                {
+                    string[] splittedInput = input.Split('+');
+                    foreach (var number in splittedInput)
+                    {
+                        int numberInt = int.Parse(number);
+                        sum = sum + numberInt;
+                    }
+                    Console.WriteLine(sum);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input, Numbers must be separated by a +");
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Entered value must be in numbers only");
             }
         }
     }
